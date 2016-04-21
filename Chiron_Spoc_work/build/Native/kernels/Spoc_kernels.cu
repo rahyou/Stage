bde438d0ab0d4e5173740cb83ce3a961c2c0fbd9
rahyou <rashousha@live.fr>
2016-04-21 14:03:52 +0200

    commit

---
+++ Chiron_Spoc_work/build/Native/kernels/Spoc_kernels.cu
@@ -0,0 +1,17 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+__global__ void vec_add(float *A, float* B,float* C,
+				int size)
+{
+  int index = blockIdx.x*blockDim.x + threadIdx.x;
+
+  if(index<size)
+    C[index] = A[index] + B[index];
+}
+
+#ifdef __cplusplus
+}
+#endif
